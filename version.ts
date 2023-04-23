3a3d7ad606ddec76efc0b578722532cb9a4d9f69
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2023-04-23 15:22:22 +0530

    chore: add warning to version file

--- version.ts
+++ version.ts
@@ -1,1 +1,8 @@
-export const LIB_VERSION = "0.2.0";
+/*
+DO NOT UPDATE THIS VALUE MANUALLY
+
+This value is automatically updated by the `prebuild` script based on the package version.
+Running the build script will automatically update this.
+*/
+
+export const LIB_VERSION = "0.2.1";
