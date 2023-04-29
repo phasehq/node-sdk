9479a854dc496ef186fe1c96ff784b81f4d7fbad
rohan-chaturvedi <rohan.chaturvedi@protonmail.com>
2023-04-29 16:56:08 +0530

    chore: clarify app id and app secret names in tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -23,23 +23,23 @@ describe("Phase", () => {
   });
 
   describe("Initialization", () => {
-    const appId =
+    const APP_ID =
       "phApp:v1:cd2d579490fd794f1640590220de86a3676fa7979d419056bc631741b320b701";
-    const appSecret =
+    const APP_SECRET =
       "pss:v1:a7a0822aa4a4e4d37919009264200ba6ab978d92c8b4f7db5ae9ce0dfaf604fe:801605dfb89822ff52957abe39949bcfc44b9058ad81de58dd54fb0b110037b4b2bbde5a1143d31bbb3895f72e4ee52f5bd:625d395987f52c37022063eaf9b6260cad9ca03c99609213f899cae7f1bb04e7";
 
     test("Should throw an error with an invalid appId", () => {
       const invalidAppId =
         "phApp:version:cd2d579490fd794f1640590220de86a3676fa7979d419056bc631741b320b701";
       expect(
-        () => new Phase(invalidAppId as any, appSecret as any)
+        () => new Phase(invalidAppId as any, APP_SECRET as any)
       ).toThrowError("Invalid Phase appID");
     });
 
     test("Should throw an error with an invalid appSecret", () => {
       const invalidAppSecret =
         "pss:v1:00000000000000000000000000000000:00000000000000000000000000000000:00000000000000000000000000000000";
-      expect(() => new Phase(appId, invalidAppSecret as any)).toThrowError(
+      expect(() => new Phase(APP_ID, invalidAppSecret as any)).toThrowError(
         "Invalid Phase AppSecret"
       );
     });
@@ -88,10 +88,10 @@ describe("Phase", () => {
     test("Check if Phase decrypt rejects the promise when the app secret is incorrect", async () => {
       const APP_ID =
         "phApp:v1:e0e50cb9a1953c610126b4092093b1beca51d08d91fc3d9f8d90482a32853215";
-      const APP_SECRET =
+      const APP_SECRET_INCORRECT =
         "pss:v1:d261abecb6708c18bebdb8b2748ee574e2b0bdeaf19b081a5f10006cc83d48d0:d146c8c6d326a7842ff9b2da0da455b3f7f568a70808e2eb0cfc5143d4fe170f:59e413612e06d75d251e3416361d0743345a9c9eda1cbcf2b1ef16e3077c011d";
 
-      const phase = new Phase(APP_ID, APP_SECRET);
+      const phase = new Phase(APP_ID, APP_SECRET_INCORRECT);
       const data = "Signal";
       const ciphertext = await phase.encrypt(data);
       expect(() => phase.decrypt(ciphertext)).rejects.toBeDefined();
